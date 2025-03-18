854127a25328d24dc26ae6a70dd0a42d46ef9e7d
Kauan Amarante <kauan.amarante@gmail.com>
2025-03-18 02:57:16 -0300

    Changed modules

---
+++ prod/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.91.0"
+    }
+  }
+}
+
+module "services" {
+  source = "./services"
+}
