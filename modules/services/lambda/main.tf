ab92b6d683e2bf995beac88d6c95ae61621f0278
Kauan Amarante <kauan.amarante@gmail.com>
2025-03-29 18:29:18 -0300

    Fix: Changed ECS & Lambda modules

--- modules/services/lambda/main.tf
+++ modules/services/lambda/main.tf
@@ -69,6 +69,57 @@ resource "aws_iam_role_policy_attachment" "this_sqs" {
   policy_arn = aws_iam_policy.this.arn
 }
 
+
+resource "aws_lb_target_group" "this" {
+  name     = var.target_group_name
+  port     = var.target_group_port
+  protocol = var.target_group_protocol
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = var.health_check_path
+    port                = var.health_check_port
+    protocol            = var.health_check_protocol
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Error"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "this" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/my-application*"]
+    }
+  }
+}
+
 resource "aws_lambda_function" "this" {
   filename      = "${path.module}/settings/sample-package.zip"
   function_name = var.lambda_function_name
